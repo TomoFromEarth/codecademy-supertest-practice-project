1eb51d7cbbe2eccf867ea41721f46cecab638193
TomoFromEarth <mikedonothackmethanks@gmail.com>
2024-07-25 03:41:34 -0700

    created basic tests for endpoints

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-require("dotenv").config();
-
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -29,8 +26,5 @@ router.post("/", function (req, res, next) {
   return res.status(400).json("user not created");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = router;
+app.use("/users", router);
+module.exports = app;
--- tests/apiTest.js
+++ tests/apiTest.js
@@ -1,6 +1,5 @@
 const request = require("supertest");
 const app = require("../app");
-const { describe } = require("node:test");
 
 describe("GET /users", () => {
   it("responds with json containing a list of all users", (done) => {
@@ -9,5 +8,70 @@ describe("GET /users", () => {
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
       .expect(200, done);
+  });
+});
+
+describe("GET /users/:id", () => {
+  it("responds with json containing a single user", (done) => {
+    request(app)
+      .get("/users/U001")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
+});
+
+describe("GET /users/:id", () => {
+  it("responds with json user not found", (done) => {
+    request(app)
+      .get("/users/idisnonesisting")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(404, "user not found")
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+describe("POST /users", () => {
+  let data = {
+    id: "1",
+    name: "John Doe",
+    contact: "1234567890",
+    address: "123 Main Street",
+  };
+  it("responds with 201 created", (done) => {
+    request(app)
+      .post("/users")
+      .send(data)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201, "user created")
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+describe("POST /users", () => {
+  let data = {
+    name: "John Doe",
+    contact: "1234567890",
+    address: "123 Main Street",
+  };
+  it("responds with 400 bad request", (done) => {
+    request(app)
+      .post("/users")
+      .send(data)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(400, "user not created")
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
   });
 });
